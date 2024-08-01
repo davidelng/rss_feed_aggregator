c3d4f95cbd58296eea7bfb6206cc7797ef59240c
davidelng <davidelngyt@gmail.com>
2024-08-01 22:57:55 +0200

    server and json handlers

--- main.go
+++ main.go
@@ -1,6 +1,34 @@
 package main
 
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
+	mux.HandleFunc("GET /v1/err", func(w http.ResponseWriter, r *http.Request) {
+		respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+	})
+
+	log.Printf("Starting server on port %s", port)
+	log.Fatal(srv.ListenAndServe())
 }
-
